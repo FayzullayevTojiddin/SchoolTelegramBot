b341ce61cb0441116d5c73fbe65ee55f5646e249
FayzullayevTojiddin <tfayzullaev66@gmail.com>
2025-04-14 09:46:21 +0200

    Config changed

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     BOT_TOKEN="7475735660:AAFZVvsOJrQHUHeThzpo0yq5W7E3PtuEzh8"
     database = {
-        'name' : "ForSchool",
+        'name' : "67fb44d924a09_forschool",
         'user' : "67fb44d924a09_admin",
         'password' : "root",
         'host' : "157.90.213.150",
