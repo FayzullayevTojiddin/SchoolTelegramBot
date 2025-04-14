a128ea49410369387cf054536e1e08edcc96ff1b
FayzullayevTojiddin <tfayzullaev66@gmail.com>
2025-04-14 09:41:35 +0200

    Config changed

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 class Config:
-    BOT_TOKEN="7475735660:AAFIYJoWZ9cMZJQxqclWdh_Iw2HtSOIbmO4"
+    BOT_TOKEN="7475735660:AAFZVvsOJrQHUHeThzpo0yq5W7E3PtuEzh8"
     database = {
         'name' : "ForSchool",
-        'user' : "root",
-        'password' : "",
-        'host' : "localhost",
+        'user' : "67fb44d924a09_admin",
+        'password' : "root",
+        'host' : "157.90.213.150",
         'port' : 3306
     }
